0de4824f176cfcb5fe54233fef90b8d4dbf0919d
Aluno Dendezeiros <aluno.den@senaiba.com.br>
2025-04-07 10:42:56 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+      
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+/*************  ✨ Codeium Command ⭐  *************/
+  /// Builds the login page with a form and a button to login.
+  ///
+  /// The form contains two text fields: one for the email and one for the
+  /// password. The button is labeled "Login" and executes the user authentication
+  /// logic when pressed.
+  ///
+  /// The form is padded on all sides with 16.0 logical pixels, and the fields
+/******  9ef45fc7-dcb9-43fd-80fa-2d5b3b96f8d3  *******/  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+
+        ),
+        ),
+        backgroundColor: Colors.blue,
+        elevation: 4,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'lib/assets/login.png',
+              width: 550,
+              height: 450,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Email',
+              prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ) 
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Senha',
+               prefixIcon: Icon(Icons.password),
+                border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica de autenticação do usuário
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
